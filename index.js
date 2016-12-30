e3375a102a5eb51b16410ecd64745d4c0c827a33
Luis F. Estevez <luis.fdez.e@gmail.com>
2016-12-30 05:33:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,6 @@
+function changeCompletely(element, index, array) {
+  array[index] = (Math.random() * 100).toString() + '!!!';
+}
+var animals = ["dog", "fish", "cat"];
+doToElementsInArray(animals, changeCompletely);
+console.log(animals);
